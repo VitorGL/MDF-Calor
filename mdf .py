450efe7a323412a1ff6d4fb5cae75621e44873e3
Vitor <vitorstormerss@gmail.com>
2019-09-24 17:31:17 -0300

    Update mdf.py
    Agora funcionando em Python

--- mdf .py
+++ mdf .py
@@ -1,49 +1,47 @@
-def main(argc, *argv[]):
+
+def main():
     h = 1
-    tempo, ap = 0
+    ap = 0
     tam = 10
-    as = None
+    ans = None
     coef_cond = 1.0
 
     while True:
-        print("Precisao:\n"
+
+        ans = int(input("Precisao:\n"
             "(1) - baixa\n"
             "(2) - media\n"
-            "(3) - alta\n")
+            "(3) - alta\n"))
 
-        as = input()
-
-        if as == 1:
+        if ans == 1:
             ap = 2.0
             break
 
-        else if as == 2:
+        elif ans == 2:
         
             ap = 4.0
             break
         
-        else if as == 3:
+        elif ans == 3:
         
             ap = 6.0
             break
         
-        else
+        else:
             print("resposta inesperada")
-    
 
     tempo = h**2/ap*coef_cond+1
 
     while tempo > h**2/ap*coef_cond:
     
-        tempo = input("Digite um valor para tempo, tal que 'tempo < {}}'".formated(h**2/ap*coef_cond))
-    
+        tempo = float(input("Digite um valor para tempo, tal que 'tempo < {}'".format(h**2/ap*coef_cond)))
 
     print("Considere o valor da condutividade termica em cm^2/s")
 
     calorMDF(h, tempo, tam, coef_cond)
 
 
-def criaMatriz(x, y, z, value)
+def criaMatriz(x, y, z, value):
 
     matriz = []
 
@@ -57,21 +55,7 @@ def criaMatriz(x, y, z, value)
     return matriz
 
 
-copiarMatriz(double ****cola, double ***copia, int x, int y, int z)
-
-    for (int i = 0 i < x ++i)
-    
-        for (int j = 0 j < y ++j)
-        
-            for (int k = 0 k < z ++k)
-            
-                (*cola)[i][j][k] = copia[i][j][k]
-            
-        
-    
-
-
-def modTempPlane(m, x, y, z, pos, temp)
+def modTempPlane(m, x, y, z, pos, temp):
 
     t = y/2
 
@@ -87,17 +71,15 @@ def modTempPlane(m, x, y, z, pos, temp)
     else:
         nk = (t - 1) / 2
 
-    for j in range(y)
-    
+    for j in range(y):
         for k in range(z):
-        
             if nj-1 < j < z-nj and nk-1 < k < z-nk:
                 m[pos][j][k] = temp
         
     return m
 
 
-calorMDF(h, tempo, dimensao, alfa)
+def calorMDF(h, tempo, dimensao, alfa):
 
     d2 = dimensao+2
     temp = 35
@@ -106,11 +88,11 @@ calorMDF(h, tempo, dimensao, alfa)
     # print("Digite a temperatura base do cubo:\n")
     # scanf("%lf", &temp)
 
-    mod_temp = input("Digite a temperatura que sera aplicada a uma area do cubo:")
+    mod_temp = float(input("Digite a temperatura que sera aplicada a uma area do cubo:"))
 
     u = modTempPlane(criaMatriz(d2, d2, d2, 35), d2, d2, d2, 0, mod_temp)
-    u2 = alocarMatriz(d2, d2, d2)
-    copiarMatriz(&u2, u, d2, d2, d2)
+
+    u2 = u[:]
 
     on = True
     c_vizinhas_som = 0
@@ -121,8 +103,7 @@ calorMDF(h, tempo, dimensao, alfa)
     fourier = alfa**2 * (tempo/h**2)
 
     while on:
-    
-        copiarMatriz(&u2, u, d2, d2, d2)
+        u2 = u[:]
 
         for i in range(dimensao):
             for j in range(dimensao):
@@ -139,7 +120,6 @@ calorMDF(h, tempo, dimensao, alfa)
                         )
 
                         u2[i][j][k] = u[i][j][k] + fourier * (c_vizinhas_som - (6 * u[i][j][k])) # Equação
-
 
         print_matriz(u2, dimensao, dimensao, dimensao)
 
@@ -163,24 +143,21 @@ calorMDF(h, tempo, dimensao, alfa)
                     valor += abs(u[i][j][k] - u2[i][j][k])
                     # print("valor = %lf = |%lf - %lf|\n\n", valor, u[i][j][k], u2[i][j][k])
                     # sleep(1)
-                
 
+        Z = valor / (d2 * d2 * d2)
 
-        Z = valor / ((d2) * (d2) * (d2))
-
-        print("Z = {}\n".formated(Z))
+        print("Z = {}\n".format(Z))
         # sleep(1)
 
-        if Z <= 0: #  || Z == Z2
+        if Z <= 0:  # || Z == Z2
         
-            print("A condicaoo de estabilidade foi atingida com valor ({}} < 0)".formated(Z))
-            on = True
+            print("A condicaoo de estabilidade foi atingida com valor ({} < 0)".format(Z))
+            on = False
         
         else:
             Z2 = Z
 
-        copiarMatriz(&u, u2, d2, d2, d2)
-    
+        u = u2[:]
 
     # FILE *arq
 
@@ -196,18 +173,20 @@ calorMDF(h, tempo, dimensao, alfa)
     #         fprint(arq, "\n")
         
     #     fprint(arq, "\n\n")
-    
 
     # fclose(arq)
 
-def print_matriz(m, x, y, z)
+
+def print_matriz(m, x, y, z):
 
     for i in range(x):
         for j in range(y):
             for k in range(z):
                 # print("M(%d)(%d)(%d) = %lf  ", i, j, k, m[i][j][k])
-                print("{}  ".formated(m[i][j][k]))
+                print("{}  ".format(m[i][j][k]))
             print()
         
         print("\n")
-    
+
+
+main()
