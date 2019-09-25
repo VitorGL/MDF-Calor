fa5c65cc25b50b940c738f4d3a8f2eebeda9ae5a
Vitor <vitorstormerss@gmail.com>
2019-09-24 21:50:35 -0300

    Update mdf.py
    Processo de criação do paralelismo por thread

--- mdf .py
+++ mdf .py
@@ -119,12 +119,10 @@ def calorMDF(h, tempo, dimensao, alfa):
                                        + u[i][j][k-1]
                         )
 
-                        u2[i][j][k] = u[i][j][k] + fourier * (c_vizinhas_som - (6 * u[i][j][k])) # Equação
+                        u2[i][j][k] = u[i][j][k] + fourier * (c_vizinhas_som - (6 * u[i][j][k]))  # Equação
 
-        print_matriz(u2, dimensao, dimensao, dimensao)
+        print_matriz(u2)
 
-        # Z = 1 / ((d2) * (d2) * (d2))
-        # print("Z = %lf\n\n", Z)
         valor = 0
 
         # for (int i = 0 i < d2 i++)
@@ -177,16 +175,119 @@ def calorMDF(h, tempo, dimensao, alfa):
     # fclose(arq)
 
 
-def print_matriz(m, x, y, z):
+def print_matriz(m):
 
-    for i in range(x):
-        for j in range(y):
-            for k in range(z):
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            for k in range(len(m[i][j])):
                 # print("M(%d)(%d)(%d) = %lf  ", i, j, k, m[i][j][k])
-                print("{}  ".format(m[i][j][k]))
+                print("{0:f}  ".format(m[i][j][k]), end="")
             print()
         
         print("\n")
 
+
+def calorMDFThread(h, tempo, dimensao, alfa):
+
+    d2 = dimensao + 2
+    temp = 35
+
+    # print("Digite a temperatura base do cubo:\n")
+    # scanf("%lf", &temp)
+
+    mod_temp = float(input("Digite a temperatura que sera aplicada a uma area do cubo:"))
+
+    u = modTempPlane(criaMatriz(d2, d2, d2, 35), d2, d2, d2, 0, mod_temp)
+
+    u2 = u[:]
+
+    on = True
+    c_vizinhas_som = 0
+    Z = 0
+    Z2 = 0
+    valor = 0
+
+    fourier = alfa ** 2 * (tempo / h ** 2)
+
+    global q
+    q = Queue()
+
+    for x in range(min(dimensao, 100)):
+        t = Thread(target=threader)
+        t.daemon = True
+        t.start()
+
+    while on:
+        u2 = u[:]
+
+        for i in range(dimensao):
+            q.put(u[i])
+
+        q.join()
+
+        print_matriz(u2)
+
+        valor = 0
+
+        for i in range(1, dimensao):
+            for j in range(1, dimensao):
+                for k in range(1, dimensao):
+                    valor += abs(u[i][j][k] - u2[i][j][k])
+
+        Z = valor / (d2 * d2 * d2)
+
+        print("Z = {}\n".format(Z))
+
+        if Z <= 0:  # || Z == Z2
+
+            print("A condicaoo de estabilidade foi atingida com valor ({} < 0)".format(Z))
+            on = False
+
+        else:
+            Z2 = Z
+
+        u = u2[:]
+
+def calculo_do_ponto(i):
+    '''Calculo da função'''
+    for j in range(dimensao):
+        for k in range(dimensao):
+            if 0 < i <= dimensao and 0 < j <= dimensao and 0 < k <= dimensao:
+                c_vizinhas_som = (u[i][j + 1][k]
+                                  + u[i][j - 1][k]
+                                  + u[i - 1][j][k]
+                                  + u[i + 1][j][k]
+                                  + u[i][j][k + 1]
+                                  + u[i][j][k - 1]
+                                  )
+
+                u2[i][j][k] = u[i][j][k] + fourier * (c_vizinhas_som - (6 * u[i][j][k]))
+
+
+
+
+def threader():
+    global q
+    while True:
+        x = q.get()
+        calculo_do_ponto(x)
+        q.task_done()
+
+# exemplo
+def runPS():
+    global q
+    q = Queue()
+
+    for x in range(100):
+        t = Thread(target=threader)
+        t.daemon = True
+        t.start()
+
+    tam = len(m) + len(m[]) + len(m[][])
+
+    for worker in range(4000, 5000):
+        q.put(worker)
+
+    q.join()
 
 main()
