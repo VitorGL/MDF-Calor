9d732440af74208d052b0d45749270fd6f38c93c
Vitor <vitorstormerss@gmail.com>
2019-09-29 21:03:04 -0300

    Update mdf.py -conflitos de referencia resolvidos

--- mdf .py
+++ mdf .py
@@ -26,27 +26,27 @@ def main():
             break
 
         elif ans == 2:
-        
+
             ap = 4.0
             break
-        
+
         elif ans == 3:
-        
+
             ap = 6.0
             break
-        
+
         else:
             print("resposta inesperada")
 
     tempo = h**2/ap*coef_cond+1
 
     while tempo > h**2/ap*coef_cond:
-    
+
         tempo = float(input("Digite um valor para tempo, tal que 'tempo < {}'".format(h**2/ap*coef_cond)))
 
     print("Considere o valor da condutividade termica em cm^2/s")
 
-    calor_mdf_thread(h, tempo, tam, coef_cond)
+    calor_mdf(h, tempo, tam, coef_cond)
 
 
 def cria_matriz(x, y, z, value):
@@ -59,7 +59,7 @@ def cria_matriz(x, y, z, value):
             matriz[i].append([])
             for k in range(z):
                 matriz[i][j].append(value)
-            
+
     return matriz
 
 
@@ -83,13 +83,13 @@ def mod_temp_plano(m, pos, temp):
         for k in range(len(m[pos][j])):
             if nj-1 < j < len(m[pos][j])-nj and nk-1 < k < len(m[pos][j])-nk:
                 m[pos][j][k] = temp
-        
+
     return m
 
 
 def calor_mdf(h, tempo, dimensao, alfa):
-    global solido
-    global solido2
+    # global solido
+    # global solido2
     global fourier
 
     d2 = dimensao+2
@@ -103,7 +103,8 @@ def calor_mdf(h, tempo, dimensao, alfa):
 
     solido = mod_temp_plano(cria_matriz(d2, d2, d2, 35), 0, mod_temp)
 
-    solido2 = solido[:]
+    # solido2 = solido[:]
+    solido2 = [[solido[i][j][:] for j in range(len(solido[i]))] for i in range(len(solido))]
 
     on = True
     c_vizinhas_som = 0
@@ -114,14 +115,15 @@ def calor_mdf(h, tempo, dimensao, alfa):
     fourier = alfa**2 * (tempo/h**2)
 
     while on:
-        solido2 = solido[:]
+        # solido2 = solido[:]
+        solido2 = [[solido[i][j][:] for j in range(len(solido[i]))] for i in range(len(solido))]
 
-        for i in range(dimensao):
-            for j in range(dimensao):
-                for k in range(dimensao):
-                
+        for i in range(1, d2-1):
+            for j in range(1, d2-1):
+                for k in range(1, d2-1):
+
                     if 0 < i <= dimensao and 0 < j <= dimensao and 0 < k <= dimensao:
-                    
+
                         c_vizinhas_som = (solido[i][j+1][k]
                                        + solido[i][j-1][k]
                                        + solido[i-1][j][k]
@@ -139,16 +141,16 @@ def calor_mdf(h, tempo, dimensao, alfa):
         # for (int i = 0 i < d2 i++)
         #     for (int j = 0 j < d2 j++)
         #         for (int k = 0 k < d2 k++)
-        #         
+        #
         #             valor += abs(solido[i][j][k] - solido2[i][j][k])
         #             # print("valor = %lf = |%lf - %lf|\n\n", valor, solido[i][j][k], solido2[i][j][k])
         #             # sleep(1)
-        #         
+        #
 
         for i in range(dimensao):
             for j in range(dimensao):
                 for k in range(dimensao):
-                
+
                     valor += abs(solido[i][j][k] - solido2[i][j][k])
                     # print("valor = %lf = |%lf - %lf|\n\n", valor, solido[i][j][k], solido2[i][j][k])
                     # sleep(1)
@@ -159,31 +161,15 @@ def calor_mdf(h, tempo, dimensao, alfa):
         # sleep(1)
 
         if Z <= 0:  # || Z == Z2
-        
+
             print("A condicaoo de estabilidade foi atingida com valor ({} < 0)".format(Z))
             on = False
-        
+
         else:
             Z2 = Z
 
-        solido = solido2[:]
-
-    # FILE *arq
-
-    # if ((arq = fopen("log.txt", "w")) == NULL)
-    #     print("Erro ao abrir arquivo de log\n")
-
-    # for (int i = 1 i <= dimensao i++)
-    
-    #     for (int j = 1 j <= dimensao j++)
-        
-    #         for (int k = 1 k <= dimensao k++)
-    #             fprint(arq, "%lf  ", solido[i][j][k])
-    #         fprint(arq, "\n")
-        
-    #     fprint(arq, "\n\n")
-
-    # fclose(arq)
+        # solido = solido2[:]
+        solido = [[solido2[i][j][:] for j in range(len(solido2[i]))] for i in range(len(solido2))]
 
 
 def print_matriz(m):
@@ -194,7 +180,7 @@ def print_matriz(m):
                 # print("M(%d)(%d)(%d) = %lf  ", i, j, k, m[i][j][k])
                 print("{0:f}  ".format(m[i][j][k]), end="")
             print()
-        
+
         print("\n")
 
 
@@ -213,7 +199,7 @@ def calor_mdf_thread(h, tempo, dimensao, alfa):
 
     solido = mod_temp_plano(cria_matriz(d2, d2, d2, 35), 0, mod_temp)
 
-    solido2 = solido[:]
+    solido2 = [[solido[i][j][:] for j in range(len(solido[i]))] for i in range(len(solido))]
 
     on = True
     c_vizinhas_som = 0
@@ -240,28 +226,28 @@ def calor_mdf_thread(h, tempo, dimensao, alfa):
     subz = int((len(solido[0][0])-2) / qntz)
 
     while on:
-        solido2 = solido[:]
+        solido2 = [[solido[i][j][:] for j in range(len(solido[i]))] for i in range(len(solido))]
         print(len(solido))
         for i in range(qntx):
             cx = (i * subx)+1
             fx = ((i * subx) + subx)
-            print(cx, fx)
+            # print(cx, fx)
             for j in range(qnty):
                 cy = (j * suby)+1
                 fy = ((j * suby) + suby)
-                print(cy, fy)
+                # print(cy, fy)
                 for k in range(qntz):
                     cz = (k * subz)+1
                     fz = ((k * subz) + subz)
-                    print(cz, fz)
+                    # print(cz, fz)
 
-                    matriz = [[solido[i][j][cz:fz] for j in range(cy, fy)] for i in range(cx, fx)]
-                    print(matriz)
+                    # matriz = [[solido[i][j][cz:fz] for j in range(cy, fy)] for i in range(cx, fx)]
+                    # print(matriz)
                     q.put([[cx, fx], [cy, fy], [cz, fz]])
 
         q.join()
 
-        print_matriz(solido2)
+        # print_matriz(solido2)
 
         valor = 0
 
@@ -282,10 +268,11 @@ def calor_mdf_thread(h, tempo, dimensao, alfa):
         else:
             Z2 = Z
 
-        solido = solido2[:]
+        solido = [[solido2[i][j][:] for j in range(len(solido2[i]))] for i in range(len(solido2))]
 
 
 def calculo_do_ponto(coords):
+    print("executei")
     cx, fx = coords[0]
     cy, fy = coords[1]
     cz, fz = coords[2]
