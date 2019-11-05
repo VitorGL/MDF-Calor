da2a7e7582fb8b998acf0fc9991e4c3f430e98c6
Vitor <vitorstormerss@gmail.com>
2019-11-05 20:19:51 -0300

    -atualização nos testes
    Não ocorre mais data race perante o while, mas  ocorre data race na equação, mesmo com o pragma critical

--- testes.c
+++ testes.c
@@ -10,81 +10,83 @@
 #include <unistd.h>
 #endif
 
-
-// int main(int argc, char const *argv[])
-// {
-//     int vetor[10];
-
-//     for (int i = 0; i < 10; ++i)
-//     {
-//         vetor[i] = 5+i;
-//     }
-
-//     int i, j;
-//     int me, n;
-
-//     #pragma omp parallel private(i,j,me,n)
-//     {
-//         me = omp_get_thread_num();
-//         n = omp_get_num_threads();
-//         printf( "Hello from %d/%d\n", me, n );
-//         #pragma omp barrier
-
-//         for ( i = 0; i < 10; i++ )
-//         {
-//             #pragma omp for
-//             for ( j = 0; j < 5; j++ )
-//             { 
-//                 printf( "thread(%d) processing %d,%d\n", me, i, j );
-//                 // printf("thread() - vetor[%d] = %d\n", i, vetor[i]);
-
-//                 #pragma omp atomic
-//                 vetor[i] += 5;
-
-//                 // usleep( 500000 );
-//             }
-//             #pragma omp barrier
-//         }
-//     }
-//     return 0;
-// }
-
+void printIt(double *m, int tam)
+{
+    for (int i = 0; i < tam; ++i)
+    {
+        printf("%.1lf ", m[i]);
+    }
+    printf("\n");
+}
 
 int main(int argc, char const *argv[])
 {
-    int tID, nT;
+    int on,
+    tID,
+    nT;
 
-    int on = 1;
+    int dimensao = 5;
 
-    while(on)
+    double valor = 0,
+    Z,
+    fourier;
+
+    double *solido = (double*) malloc(dimensao * sizeof(double));
+    double *solido2 = (double*) malloc(dimensao * sizeof(double));
+
+    for (int i = 0; i < dimensao; ++i)
     {
-        #pragma omp parallel default(none) private(tID, nT) shared(on) num_threads(3)
+        solido[i] = 10;
+        solido2[i] = 10;
+    }
+    solido[0] = 80;
+    solido2[0] = 80;
+
+    #pragma omp parallel default(none) private(tID, nT, on, dimensao, fourier) shared(solido, solido2) num_threads(3)
+    {
+        fourier = pow(1.0, 2) * (0.1/pow(1, 2));
+        dimensao = 5;
+
+        on = 1;
+        tID = omp_get_thread_num();
+        nT = omp_get_num_threads();
+
+        printf("thread %d de %d.\n", tID+1, nT);
+
+        #pragma omp barrier
+
+        while(on)
         {
-            tID = omp_get_thread_num();
-            nT = omp_get_num_threads();
+            #pragma omp for
+            for (int i = 1; i < dimensao-1; ++i)
+            {
+                printf("thread %d no for %d da iteracao %d.\n", tID+1, i, on);
+                // #pragma omp critical
+                // solido2[i] = solido[i] + (fourier * (solido[i+1] - (2 * solido[i]) + solido[i-1]));
+            }
 
-            printf("thread(%d) - while[%d]\n", tID, on);
-            #pragma omp barrier
-
-            // exit(0);
             #pragma omp single
             {
-                on++;
-                if (on == 4)
-                {
-                    on = 0;
-                    
-                }
+                printf("somente a thread %d no bloco single da iteracao %d.\n", tID+1, on);
             }
-            #pragma omp barrier
+            if (on < 3)
+            {
+                on++;
+            }
+            else
+                on = 0;
         }
     }
+
+    free(solido);
+    free(solido2);
     return 0;
 }
 
+
 // int main(int argc, char const *argv[])
 // {
-//     int on = 0, tID;
+//     int on, tID, nT;
 //     int dimensao = 10;
 //     double valor = 0, Z;
 
@@ -102,53 +104,64 @@ int main(int argc, char const *argv[])
 //     double fourier = pow(1.0, 2) * (0.1/pow(1, 2));
 
 
-//     #pragma omp parallel private(on, tID)
+//     #pragma omp parallel default(none) private(tID, nT, on, dimensao, fourier) shared(solido, solido2) num_threads(3)
 //     {
+//         dimensao = 10;
+//         fourier = pow(1.0, 2) * (0.1/pow(1, 2));
+
+//         on = 1;
 //         tID = omp_get_thread_num();
-//         int n = omp_get_num_threads();
-//         printf( "Hello from %d/%d\n", tID, n );
+//         nT = omp_get_num_threads();
+//         printf("thread %d de %d.\n", tID+1, nT);
 
 //         #pragma omp barrier
-//         for (on = 0; on < 100; on++)
+
+//         while (on)
 //         {
 //             #pragma omp for
-//             for (int i = 1; i < dimensao; i++)
+//             for (int i = 1; i < dimensao-1; i++)
 //             {
-//                 if (0 < i && i < dimensao-1)
-//                 {
-//                     printf( "thread(%d) processing %d,%d\n", tID, i, on);
+//                 printf("thread %d no for %d da iteracao %d.\n", tID+1, i, on);
 
-//                     #pragma omp critical
-//                     solido2[i] = solido[i] + (fourier * (solido[i+1] - (2 * solido[i]) + solido[i-1]));
-//                 }
+//                 #pragma omp critical
+//                 solido2[i] = solido[i] + (fourier * (solido[i+1] - (2 * solido[i]) + solido[i-1]));
 //             }
-//             #pragma omp barrier
 
 //             #pragma omp single
 //             {
 //                 printIt(solido2, dimensao);
 //                 printIt(solido, dimensao);
+//             }
 
-//                 valor = 0;
-                
-//                 // #pragma omp for reduction(+: valor)
-//                 for (int i = 1; i < dimensao; i++)
-//                 {
-//                     valor += abs(solido2[i] - solido[i]);
-//                 }
+//             // valor = 0;
+            
+//             // // #pragma omp for reduction(+: valor)
+//             // for (int i = 1; i < dimensao; i++)
+//             // {
+//             //     valor += abs(solido2[i] - solido[i]);
+//             // }
 
-//                 Z = valor / (dimensao);
+//             // Z = valor / (dimensao);
 
-//                 printf("Z = %lf\n\n", Z);
+//             // printf("Z = %lf\n\n", Z);
 
-//                 if (Z <= 0)
-//                 {
-//                     printf("%lf < 0\n", Z);
-//                     on = 100;
-//                 }
-//                 else
-//                     on--;
+//             // if (Z <= 0)
+//             // {
+//             //     printf("%lf < 0\n", Z);
+//             //     on = 0;
+//             // }
 
+//             if (on < 3)
+//             {
+//                 on++;
+//             }
+//             else
+//             {
+//                 on = 0;
+//             }
+
+//             #pragma omp single
+//             {
 //                 for (int i = 0; i < dimensao; ++i)
 //                 {
 //                     solido[i] = solido2[i];
